f8a41855e47ffb0769539fb99c186482b2299797
VioletLing <50896903+VioletLing@users.noreply.github.com>
2021-01-15 11:37:12 +0800

    Beta V1.1

--- functions.py
+++ functions.py
@@ -1,12 +1,34 @@
-import xlrd
-import xlwt
-import tkinter as tk
 from tkinter import ttk
 
+import xlrd
+import xlwt
+from tkinter import *
+import win32ui
+import win32con
+
+file_type = 'Excel文件(*.xls)|*.xls|' \
+            'All File(*.*)|*.*|' \
+            '|'
+
+API_flag = win32con.OFN_OVERWRITEPROMPT | win32con.OFN_FILEMUSTEXIST
+
+
+def Win_Save_File():  # 保存文件时，文件后缀需要另处理
+    print("Save File\n")
+    dlg = win32ui.CreateFileDialog(0, None, None, API_flag, file_type)  # 指定为保存文件窗口
+    dlg.SetOFNInitialDir('C:')  # 默认打开的位置
+    dlg.DoModal()
+    path = dlg.GetPathName()  # 获取打开的路径
+    return path
 
 
 def merge(file_0, file_1, cow0_0, cow0_1, cow1_0, cow1_1, display):
     print("正在合并文件！")
+
+    fileName = Win_Save_File()
+    if len(str(fileName)) == 0:
+        return 0
+    fileName = str(fileName) + ".xls"
 
     workbook = xlwt.Workbook(encoding='ascii')
     worksheet = workbook.add_sheet('My Worksheet')
@@ -18,125 +40,108 @@ def merge(file_0, file_1, cow0_0, cow0_1, cow1_0, cow1_1, display):
     sum = int(0)
     if cow1_0 == "无":
         for i in range(file_0_len):
-            master = file_0.cell(i, int(cow0_0)-1).value
+            master = file_0.cell(i, int(cow0_0) - 1).value
             for j in range(file_1_len):
-                slave = file_1.cell(j, int(cow0_1)-1).value
-                if master==slave:
+                slave = file_1.cell(j, int(cow0_1) - 1).value
+                if master == slave:
                     for n in range(file_0_wid):
                         worksheet.write(sum, n, label=file_0.cell(i, n).value)
                     for m in range(file_1_wid):
-                        worksheet.write(sum, int(file_0_wid)+int(m), label=file_1.cell(i, m).value)
+                        worksheet.write(sum, int(file_0_wid) + int(m), label=file_1.cell(i, m).value)
                     display.insert('end', master)
                     display.insert('end', "\n")
-                    sum = sum+1
+                    sum = sum + 1
                     break
     else:
         for i in range(file_0_len):
-            master0 = file_0.cell(i, int(cow0_0)-1).value
+            master0 = file_0.cell(i, int(cow0_0) - 1).value
             master1 = file_0.cell(i, int(cow1_0) - 1).value
             for j in range(file_1_len):
-                slave0 = file_1.cell(j, int(cow0_1)-1).value
+                slave0 = file_1.cell(j, int(cow0_1) - 1).value
                 slave1 = file_1.cell(j, int(cow1_1) - 1).value
-                if master0==slave0 and master1==slave1:
+                if master0 == slave0 and master1 == slave1:
                     for n in range(file_0_wid):
                         worksheet.write(sum, n, label=file_0.cell(i, n).value)
                     for m in range(file_1_wid):
-                        worksheet.write(sum, int(file_0_wid)+int(m), label=file_1.cell(i, m).value)
+                        worksheet.write(sum, int(file_0_wid) + int(m), label=file_1.cell(i, m).value)
                         display.insert('end', master0)
                         display.insert('end', "\n")
-                    sum = sum+1
+                    sum = sum + 1
                     break
 
-    workbook.save('合并结果.xls')
-
-
-
-def P1():
-    data = xlrd.open_workbook("山东省产业高技能人才培育情况调研问卷（院校高技能人才培训基地） (1)(1).xlsx")
-    area = data.sheet_by_name(u'培训')
-
-    workbook = xlwt.Workbook(encoding='ascii')
-    worksheet = workbook.add_sheet('My Worksheet')
-
-    for i in range(2, 239):
-        master = area.cell(i, 15).value
-        temp = str(master)
-        print(temp)
-        temp.replace("\n", ";")
-        worksheet.write(i, 0, label=temp)
-
-    workbook.save('Excel_Workbook.xls')
+    workbook.save(fileName)
+    display.insert('end', "文件合并成功，保存在")
+    display.insert('end', fileName)
 
 
 def MainSolution(file_0, file_1):
-    window = tk.Tk()
+    window = Tk()
     window.title("加载完毕...")
     window.geometry("1000x600")
     file_0_sheet = file_0.sheet_names()
     file_1_sheet = file_1.sheet_names()
-    SNFL = tk.Label(window, text="第一个文件需要加载的Sheet")
-    SNFL.place(x=40, y=20)
-    SNFT = tk.Label(window, text=file_0_sheet[0])
-    SNFT.place(x=200, y=20)
-    SNSL = tk.Label(window, text="第二个文件需要加载的Sheet")
-    SNSL.place(x=40, y=40)
-    SNST = tk.Label(window, text=file_1_sheet[0])
-    SNST.place(x=200, y=40)
+    Label(window, text="文件基本信息").place(x=465, y=1)
+
+    Label(window, text="第一个文件需要加载的Sheet").place(x=40, y=20)
+    Label(window, text=file_0_sheet[0]).place(x=200, y=20)
+    Label(window, text="第二个文件需要加载的Sheet").place(x=40, y=40)
+    Label(window, text=file_1_sheet[0]).place(x=200, y=40)
 
     sheet_0 = file_0.sheet_by_name(str(file_0_sheet[0]))
     sheet_1 = file_1.sheet_by_name(str(file_1_sheet[0]))
-    TFL = tk.Label(window, text="第一个表的总行数")
-    TFL.place(x=40, y=60)
-    TFT = tk.Label(window, text=sheet_0.nrows)
-    TFT.place(x=200, y=60)
-    TSL = tk.Label(window, text="第二个表的总行数")
-    TSL.place(x=40, y=80)
-    TST = tk.Label(window, text=sheet_1.nrows)
-    TST.place(x=200, y=80)
 
-    FFT = tk.Label(window, text="选取第一个文件的第一个基准组")
-    FFT.place(x=40, y=100)
-    xVariable0 = tk.StringVar()  # #创建变量，便于取值
+    Label(window, text="第一个表的总行数").place(x=40, y=60)
+    Label(window, text=sheet_0.nrows).place(x=200, y=60)
+
+    Label(window, text="第一个表的总列数").place(x=240, y=60)
+    Label(window, text=sheet_0.ncols).place(x=400, y=60)
+
+    Label(window, text="第二个表的总行数").place(x=40, y=80)
+    Label(window, text=sheet_1.nrows).place(x=200, y=80)
+
+    Label(window, text="第二个表的总列数").place(x=240, y=80)
+    Label(window, text=sheet_1.ncols).place(x=400, y=80)
+
+    Label(window, text="合并文件设置").place(x=465, y=120)
+
+    Label(window, text="选取第一个文件的第一个基准组").place(x=40, y=160)
+    xVariable0 = StringVar()  # #创建变量，便于取值
     FF0 = ttk.Combobox(window, textvariable=xVariable0)  # #创建下拉菜单
-    FF0.place(x=240, y=100)  # #将下拉菜单绑定到窗体
+    FF0.place(x=240, y=160)  # #将下拉菜单绑定到窗体
     FF0["value"] = ("1", "2", "3", "4", "5", "6", "7", "8", "9")  # #给下拉菜单设定值
     FF0.current(0)
 
-    SFT = tk.Label(window, text="选取第二个文件的第一个基准组")
-    SFT.place(x=480, y=100)
-    xVariable1 = tk.StringVar()  # #创建变量，便于取值
+    SFT = Label(window, text="选取第二个文件的第一个基准组")
+    SFT.place(x=480, y=160)
+    xVariable1 = StringVar()  # #创建变量，便于取值
     FF1 = ttk.Combobox(window, textvariable=xVariable1)  # #创建下拉菜单
-    FF1.place(x=680, y=100)  # #将下拉菜单绑定到窗体
+    FF1.place(x=680, y=160)  # #将下拉菜单绑定到窗体
     FF1["value"] = ("1", "2", "3", "4", "5", "6", "7", "8", "9")  # #给下拉菜单设定值
     FF1.current(0)
 
-
-    FST = tk.Label(window, text="选取第一个文件的第二个基准组")
-    FST.place(x=40, y=120)
-    xVariable2 = tk.StringVar()  # #创建变量，便于取值
+    FST = Label(window, text="选取第一个文件的第二个基准组")
+    FST.place(x=40, y=180)
+    xVariable2 = StringVar()  # #创建变量，便于取值
     FS0 = ttk.Combobox(window, textvariable=xVariable2)  # #创建下拉菜单
-    FS0.place(x=240, y=120)  # #将下拉菜单绑定到窗体
-    FS0["value"] = ("无","1", "2", "3", "4", "5", "6", "7", "8", "9")  # #给下拉菜单设定值
+    FS0.place(x=240, y=180)  # #将下拉菜单绑定到窗体
+    FS0["value"] = ("无", "1", "2", "3", "4", "5", "6", "7", "8", "9")  # #给下拉菜单设定值
     FS0.current(0)
 
-    FST = tk.Label(window, text="选取第二个文件的第二个基准组")
-    FST.place(x=480, y=120)
-    xVariable3 = tk.StringVar()  # #创建变量，便于取值
+    FST = Label(window, text="选取第二个文件的第二个基准组")
+    FST.place(x=480, y=180)
+    xVariable3 = StringVar()  # #创建变量，便于取值
     FS1 = ttk.Combobox(window, textvariable=xVariable3)  # #创建下拉菜单
-    FS1.place(x=680, y=120)  # #将下拉菜单绑定到窗体
-    FS1["value"] = ("无","1", "2", "3", "4", "5", "6", "7", "8", "9")  # #给下拉菜单设定值
+    FS1.place(x=680, y=180)  # #将下拉菜单绑定到窗体
+    FS1["value"] = ("无", "1", "2", "3", "4", "5", "6", "7", "8", "9")  # #给下拉菜单设定值
     FS1.current(0)
 
-    info = tk.Label(window, text="合并日志")
-    info.place(x=270, y=250)
+    SM = Button(window, text='开始合并文件',
+                command=lambda: merge(sheet_0, sheet_1, FF0.get(), FF1.get(), FS0.get(), FS1.get(), message_display))
+    SM.place(x=80, y=220)
 
-    message_display = tk.Text(window)
+    Label(window, text="合并日志").place(x=270, y=250)
+
+    message_display = Text(window)
     message_display.place(x=40, y=275)
-
-    SM = tk.Button(window, text='开始合并文件', command=lambda: merge(sheet_0, sheet_1, FF0.get(), FF1.get(), FS0.get(), FS1.get(), message_display))
-    SM.place(x=80, y=160)
-
-
-
 
     window.mainloop()
--- main.py
+++ main.py
@@ -21,7 +21,9 @@ def Win_Open_File_0():
     dlg.DoModal()
     path = dlg.GetPathName()
     file_0 = str(path)
+    fileName_0.insert('end', file_0)
     print(file_0)
+
 
 def Win_Open_File_1():
     global file_1
@@ -30,20 +32,30 @@ def Win_Open_File_1():
     dlg.DoModal()
     path = dlg.GetPathName()
     file_1 = str(path)
+    fileName_1.insert('end', file_1)
     print(file_1)
 
-def Load_Excel():  # 保存文件时，文件后缀需要另处理
-    data0 = xlrd.open_workbook(file_0)
-    data1 = xlrd.open_workbook(file_1)
-    print(data0.sheet_names())
-    print(data1.sheet_names())
-    functions.MainSolution(data0, data1)
+
+def Load_Excel():
+    try:
+        data0 = xlrd.open_workbook(file_0)
+        data1 = xlrd.open_workbook(file_1)
+        print(data0.sheet_names())
+        print(data1.sheet_names())
+        functions.MainSolution(data0, data1)
+    except:
+        tkinter.messagebox.showerror('加载失败', '请检查文件是否被其他程序占用或未选取文件！')
+
 
 def test(a):
     sheet = a.sheet_names()
     print(sheet)
+
+
 def opensource():
-    webbrowser.open("www.github.com")
+    webbrowser.open("https://github.com/VioletLing/ExcelTools")
+
+
 def intro():
     introduction = Tk()
     introduction.title("软件说明")
@@ -57,15 +69,20 @@ def intro():
     Label(introduction, text="6.确定后点击#开始合并文件#，合并后的文件将保存在软件目录下").pack()
     Label(introduction, text="其他问题请联系作者Violetnris@outlook.com").pack()
 
+
 MainWindows = Tk()  # 主窗体
 MainWindows.title("合并Excel小工具 Beta V1.0")
 MainWindows.geometry("500x300")
 menubar = Menu(MainWindows)
-MainWindows.config(menu = menubar)
-menubar.add_command(label = "查看源代码", command = lambda:opensource())
-menubar.add_command(label = "说明", command = lambda:intro())
-Button(text='选择第一个文件', command=Win_Open_File_0).pack()
-Button(text='选择第二个文件', command=Win_Open_File_1).pack()
-Button(text='加载文件', command=lambda:Load_Excel()).pack()
+MainWindows.config(menu=menubar)
+menubar.add_command(label="查看源代码", command=lambda: opensource())
+menubar.add_command(label="说明", command=lambda: intro())
+Button(text='选择第一个文件', command=lambda: Win_Open_File_0()).place(x=390, y=40)
+fileName_0 = Text(MainWindows, width=50, height=2)
+fileName_0.place(x=10, y=40)
+Button(text='选择第二个文件', command=lambda: Win_Open_File_1()).place(x=390, y=70)
+fileName_1 = Text(MainWindows, width=50, height=2)
+fileName_1.place(x=10, y=70)
+Button(text='加载文件', command=lambda: Load_Excel()).place(x=240, y=120)
 
 MainWindows.mainloop()
